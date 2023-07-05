8a9234fe296c3fdfd8c0767b0352adea9ca16b9b
Ashok Kandpal <75152443+AshokKandpal2001@users.noreply.github.com>
2023-07-05 20:01:11 +0530

    Add files via upload
    Final commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hoursHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegree = (seconds / 60) * 360 + 90;
+    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+    console.log(seconds);
+
+    const mins = now.getMinutes();
+    const minsDegree = (mins / 60) * 360 + 90;
+    minsHand.style.transform = `rotate(${minsDegree}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegree = (hours / 12) * 360 + 90;
+    hoursHand.style.transform = `rotate(${hoursDegree}deg)`;
+}
+
+setInterval(setDate, 1000);
